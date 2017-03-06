ac33a98b734e2ea692a73655f0143d6e2dfc4690
iaDRM <drm@localhost>
2017-03-05 21:19:04 -0500

    Issue #77: Errors when trying to setup on MacOS X Sierra

--- prepare-mac-os.sh
+++ prepare-mac-os.sh
@@ -10,12 +10,49 @@ install_libusb() {
 }
 
 if [ -e "/opt/local/bin/port" ]; then
-	sudo port install wget
-	sudo port install libelf
-	sudo port install libmpc
-	sudo port install libusb
-	
+	if [[ $UID != 0 ]]; then
+	    echo "Please run this script with sudo"
+	    exit 1
+	fi
+
+	port install wget
+	port install libelf
+	port install libmpc
+	port install libusb
+	port install autoconf
+	port install automake
+	port install libtool
+	port install cmake
+	port install libsdl
+	port install doxygen
+	port install readline
+
+	port install gcc49
+	port install gcc_select
+
+	# Build with clang
+	port select --set gcc none
+
 	install_libusb
+
+	# Switch to GNU variants
+	mkdir gnubin
+
+	port select --set gcc mp-gcc49
+
+	port install gsed
+	ln -s /opt/local/bin/gsed gnubin/sed
+
+	port install gmake
+	ln -s /opt/local/bin/gmake gnubin/make
+
+	port install libtool
+	ln -s /opt/local/bin/glibtoolize gnubin/libtoolize
+
+	echo "Note: MacPort's GCC selected."
+	echo "      To switch back to clang, run: sudo port select --set gcc none"
+	echo "      To switch back to MacPort's gcc, run: port select --set gcc mp-gcc49"
+
 elif [ -e "/usr/local/bin/brew" ]; then
 	CURRENT_USER=$(stat -f '%Su' /dev/console)
 	sudo -u $CURRENT_USER brew install wget
--- toolchain-sudo.sh
+++ toolchain-sudo.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # toolchain-sudo.sh by Dan Peori (danpeori@oopo.net)
+
+ if [[ $UID != 0 ]]; then
+   echo "Please run this script with sudo"
+   exit 1
+ fi
+
+ # TODO: Add checks to require gnu sed, make, and gcc
  
  INSTALLDIR=/usr/local/pspdev
  
@@ -8,7 +15,7 @@
 
  ## Set up the environment.
  export PSPDEV=$INSTALLDIR
- export PATH=$PATH:$PSPDEV/bin
+ export PATH=`pwd`/gnubin:$PATH:$PSPDEV/bin
 
  ## Run the toolchain script.
  ./toolchain.sh $@ || { echo "ERROR: Could not run the toolchain script."; exit 1; }
